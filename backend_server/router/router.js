6f965c16b61dfc750703102c09207a10a3abd9d1
tycowles <tyleracowles@gmail.com>
2024-06-17 18:23:29 -0400

    guide portal router

--- backend_server/router/router.js
+++ backend_server/router/router.js
@@ -12,11 +12,25 @@ const website_controls = require('./../controller/website')
 // Ex: router.get('/user', controller.function <- so when 
 // localhost:3001/api/getClietn called with get request we want to return a function or page);
 
+// client side
 router.post('/accountInfo', website_controls.retrieveClientInfo);
 router.post('/newClient', website_controls.insertClientAccounts);
 router.post('/updateClient', website_controls.updateClientInfo);
 router.post('/allGuides', website_controls.getAllGuides);
 router.post('/guideInfo', website_controls.getGuideInfo);
 router.post('/filterGuides', website_controls.getGuidesByFilter);
+
+// guide portal 
+router.post('/retGuideInfo', portal_controls.retrieveGuideInfo);
+router.post('/newGuide', portal_controls.insertGuideAccounts);
+router.post('/updateGuide', portal_controls.updateGuideAccounts);
+router.post('/newHobby', portal_controls.addHobbies);
+router.post('/newInvolvement', portal_controls.addInvolvement);
+router.post('/remHobby', portal_controls.deleteHobby);
+router.post('/remInvolvement', portal_controls.deleteInvolvement);
+
+
+
+
 //Export for app.js
 module.exports = router;
